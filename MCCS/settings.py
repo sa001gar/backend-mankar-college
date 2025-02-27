b33ca6734e856c8cdfe01d8b925bd0bb7bb66342
sa001gar <kundusagar233@gmail.com>
2025-02-28 00:06:51 +0530

    update to all allowed host

--- MCCS/settings.py
+++ MCCS/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost','13.233.152.122','https://redesign-mccs.sagarkundu.live/',]
+# ALLOWED_HOSTS = ['127.0.0.1','localhost','13.233.152.122','https://redesign-mccs.sagarkundu.live/',]
+ALLOWED_HOSTS = ['*']
 CSRF_COOKIE_SECURE = True
 CSRF_TRUSTED_ORIGINS = ['https://redesign-mccs.sagarkundu.live',]
 
