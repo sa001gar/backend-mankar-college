1d4f49e5bbd86e2d1d885df3b6437a0f479a18c8
sa001gar <kundusagar233@gmail.com>
2025-05-11 01:07:58 +0530

    Update DEBUG setting to False in settings.py and change PDF.js worker source in syllabus.html

--- MCCS/settings.py
+++ MCCS/settings.py
@@ -29,7 +29,7 @@ load_dotenv(os.path.join(BASE_DIR, ".env"))
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ['127.0.0.1','localhost','13.233.152.122','https://redesign-mccs.sagarkundu.live/',]
 ALLOWED_HOSTS = ['*']
