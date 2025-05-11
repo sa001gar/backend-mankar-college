756f5e62d3bd927ca3635b3885241831425174ae
sa001gar <kundusagar233@gmail.com>
2025-05-11 15:21:24 +0530

    Update

--- MCCS/settings.py
+++ MCCS/settings.py
@@ -14,12 +14,9 @@ from dotenv import load_dotenv
 from pathlib import Path
 
 import dj_database_url
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(os.path.join(BASE_DIR, ".env"))
-
 
 
 # Quick-start development settings - unsuitable for production
