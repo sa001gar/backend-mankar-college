7f941558e328f406e943adb2fb415f124a809f09
sa001gar <kundusagar233@gmail.com>
2025-05-12 21:30:43 +0530

    fix: Update media URL and enhance 500 error page layout and styling

--- MCCS/settings.py
+++ MCCS/settings.py
@@ -156,7 +156,7 @@ STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Media files settings
-MEDIA_URL = 'https://media.computersciencemancoll.in/media/'
+MEDIA_URL = 'https://media.computersciencemancoll.in/'
 MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
